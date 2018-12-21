4d271b97f47aed1bd918831a696299ebd96760c8
Jean Tessier <jean@jeantessier.com>
2018-12-21 14:46:51 -0800

    Use SLF4J Test to mock logging
    Based on the
    [Grails Mock Logging with Slf4j 
    Test](http://guides.grails.org/grails-mock-logging-slf4j-test/guide/index.html)
    guide.

--- src/test/groovy/myloggingcontroller/LoggingControllerSpec.groovy
+++ src/test/groovy/myloggingcontroller/LoggingControllerSpec.groovy
@@ -2,12 +2,18 @@ package myloggingcontroller
 
 import grails.testing.web.controllers.ControllerUnitTest
 import org.slf4j.Logger
+import spock.lang.Shared
 import spock.lang.Specification
+import uk.org.lidalia.slf4jext.Level
+import uk.org.lidalia.slf4jtest.TestLoggerFactory
 
 class LoggingControllerSpec extends Specification implements ControllerUnitTest<LoggingController> {
 
-    def setup() {
-        controller.log = Mock(Logger)
+    @Shared
+    def logger = TestLoggerFactory.getTestLogger(LoggingController)
+
+    def cleanup() {
+        TestLoggerFactory.clear()
     }
 
     void "test something"() {
@@ -15,7 +21,8 @@ class LoggingControllerSpec extends Specification implements ControllerUnitTest<
         controller.index()
 
         then:
-        1 * controller.log.error(_)
+        logger.loggingEvents.size() == 1
+        logger.loggingEvents[0].level == Level.ERROR
     }
 
 }
